bba75c64a7bede287f944680b391680c7e950101
Brian Hurlow <brian@brianhurlow.com>
2016-04-11 15:13:38 -0400

    README updates

--- README.md
+++ README.md
@@ -1,9 +1,25 @@
 # imageinfo
 
-extract some image info
+extract image type and size from an inputstream
+
+## Installation
+
+add to your project dependencies
+```
+[imageinfo "0.0.2"]
+```
 
 ## Usage
 
+currently exposes only 1 function `info`:
+
+```
+(use 'imageinfo)
+
+(info (io/input-stream "test/imageinfo/test_images/png/42x165.png"))
+
+=> {:width 42, :height 165, :type :png}
+```
 
 ## License
 
--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject imageinfo "0.0.2"
   :main imageinfo.core
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "extract image type and dimensions from an inputstream"
+  :url "https://github.com/bhurlow/clj-imageinfo"
   :license {:name "Eclipse Public License" :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
